67de5ef31908cb4cf4b983a8908c52b6284b145e
Byran Wills-Heath <byran@adgico.co.uk>
2020-01-29 17:40:13 +0000

    Add selenium for chrome

--- features/environment.py
+++ features/environment.py
@@ -1,5 +1,11 @@
 from behave import runner
+from selenium import webdriver
 
 def before_all(context : runner.Context):
     print('all')
     context.aaa = 'helen'
+
+    context.browser = webdriver.Chrome()
+
+def after_all(contest: runner.Context):
+	context.browser.quit()
