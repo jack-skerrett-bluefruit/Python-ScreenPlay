4ad1a501930464bc7925f977cb42aa4113402ef1
Byran Wills-Heath <byran@adgico.co.uk>
2020-01-29 22:34:05 +0000

    Add type hints to step handlers

--- features/steps/SearchingOnGoogle.py
+++ features/steps/SearchingOnGoogle.py
@@ -8,18 +8,18 @@ from selenium.webdriver.common.keys import Keys
 # use_step_matcher("re")
 
 @step(u'Byran has opened Google')
-def step_impl(context):
+def step_impl(context: runner.Context):
     context.active_actor = Actor.named('Byran')
     context.browser.get('https://www.google.co.uk')
 
 
 @step(u'they search for "{search_text}"')
-def step_impl(context, search_text: str):
+def step_impl(context: runner.Context, search_text: str):
     browser: webdriver.Chrome = context.browser
     search_box: WebElement = browser.find_element_by_name('q')
     search_box.send_keys('Hello World', Keys.ENTER)
     print('hello world')
 
 @step(u'they should see results for "{expected}" programs')
-def step_impl(context, expected: str):
+def step_impl(context: runner.Context, expected: str):
     pass
