009dbe13d12ed5605fa289388470af16697ea728
Byran Wills-Heath <byran@adgico.co.uk>
2020-01-31 14:36:58 +0000

    Switch Chrome to run headless

--- abilities/browse_the_web.py
+++ abilities/browse_the_web.py
@@ -1,11 +1,12 @@
 from screenplay.ability import Ability
 from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.remote.webdriver import WebDriver
 
 
 class browse_the_web(Ability):
-    def __init__(self, browserType: type):
-        self._browserType = browserType
+    def __init__(self, create_browser_function: type):
+        self._create_browser_function = create_browser_function
         self._webdriver = None
 
     def clean_up(self):
@@ -15,9 +16,15 @@ class browse_the_web(Ability):
     @property
     def browser(self) -> WebDriver:
         if self._webdriver == None:
-            self._webdriver = self._browserType()
+            self._webdriver = self._create_browser_function()
         return self._webdriver
 
     @staticmethod
+    def _create_Chrome_browser():
+        chrome_options = ChromeOptions()
+        chrome_options.add_argument('--headless')
+        return Chrome(chrome_options=chrome_options)
+
+    @staticmethod
     def using_Chrome():
-        return browse_the_web(Chrome)
+        return browse_the_web(browse_the_web._create_Chrome_browser)
