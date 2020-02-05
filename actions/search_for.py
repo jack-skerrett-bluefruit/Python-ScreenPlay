68e2c5db8d55582fac739b3c843cac508f3833cd
Byran Wills-Heath <byran@adgico.co.uk>
2020-02-05 21:18:00 +0000

    Use page objects to locate elements

--- actions/search_for.py
+++ actions/search_for.py
@@ -1,6 +1,7 @@
 from screenplay import Action, Actor
 from tasks.enter_text import enter_text
-from tasks.send_key import send_enter_key_to_element_with_name
+from tasks.send_key import send_enter_key_to
+from pages.google_homepage import google_homepage
 
 
 class search_for(Action):
@@ -9,6 +10,6 @@ class search_for(Action):
 
     def perform_as(self, actor: Actor):
         actor.attempts_to(
-            enter_text(self._text).into_textbox_with_name('q'),
-            send_enter_key_to_element_with_name('q')
+            enter_text(self._text).into(google_homepage.search_textbox),
+            send_enter_key_to(google_homepage.search_textbox)
             )
--- tasks/send_key.py
+++ tasks/send_key.py
@@ -5,29 +5,15 @@ from selenium.webdriver.common.keys import Keys
 from abilities.browse_the_web import browser_for
 
 
-class _send_key_to_element_with_id_task(Task):
-    def __init__(self, id: str, key: Keys):
-        self._id = id
+class _send_key_to_element(Task):
+    def __init__(self, locator, key: Keys):
+        self._locator = locator
         self._key = key
 
     def perform_as(self, actor: Actor):
-        element: WebElement = browser_for(actor).find_element_by_id(self._id)
+        element: WebElement = browser_for(actor).find_element(*self._locator)
         element.send_keys(self._key)
 
 
-class _send_key_to_element_with_name_task(Task):
-    def __init__(self, name: str, key: Keys):
-        self._name = name
-        self._key = key
-
-    def perform_as(self, actor: Actor):
-        element: WebElement = browser_for(actor).find_element_by_name(self._name)
-        element.send_keys(self._key)
-
-
-def send_enter_key_to_element_with_id(id: str):
-    return _send_key_to_element_with_id_task(id, Keys.ENTER)
-
-
-def send_enter_key_to_element_with_name(name: str):
-    return _send_key_to_element_with_name_task(name, Keys.ENTER)
+def send_enter_key_to(locator):
+    return _send_key_to_element(locator, Keys.ENTER)
