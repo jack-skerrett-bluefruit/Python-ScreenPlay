85881a299465ec9e6e0c93a748c340d61fb8e62a
Byran Wills-Heath <byran@adgico.co.uk>
2020-02-05 21:29:23 +0000

    Add page object for search results

---
+++ pages/google_search_results.py
@@ -0,0 +1,4 @@
+from selenium.webdriver.common.by import By
+
+class google_search_results:
+    titles = (By.TAG_NAME, 'h3')
--- questions/the_search_result_titles.py
+++ questions/the_search_result_titles.py
@@ -1,7 +1,8 @@
 from screenplay import Actor, Question
 from abilities.browse_the_web import browser_for
+from pages.google_search_results import google_search_results
 
 class the_search_result_titles(Question):
     def answered_by(self, actor: Actor):
-        elements = browser_for(actor).find_elements_by_tag_name('h3')
+        elements = browser_for(actor).find_elements(*google_search_results.titles)
         return [element.text for element in elements]
