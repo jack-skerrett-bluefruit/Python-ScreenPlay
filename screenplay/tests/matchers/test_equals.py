e0ebf1136654f9834cff1cec2f5a501858fb7ec3
Byran Wills-Heath <byran@adgico.co.uk>
2020-02-15 22:12:16 +0000

    Add tests for built in screen play matchers

---
+++ screenplay/tests/matchers/test_equals.py
@@ -0,0 +1,13 @@
+from screenplay.matchers.equals import equals
+
+def test_The_equals_matcher_returns_True_if_the_answer_is_equal_to_the_expected():
+    assert equals(1).matches(1), "Two equal values were not classed as equal"
+
+def test_The_equals_matcher_returns_False_if_the_answer_is_not_equal_to_the_expected():
+    assert equals(2).matches(1) == False, "Two different values were classed as equal"
+
+def test_The_equals_matcher_failure_message_indicates_the_failure():
+    matcher = equals(3)
+    matcher.matches(4)
+
+    assert matcher._fail_message == '4 is not equal to 3'
