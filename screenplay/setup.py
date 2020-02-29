ac0b0173a1caf36e9a0bd3c64a76dcb6bfd03d57
Byran Wills-Heath <byran@adgico.co.uk>
2020-02-29 15:25:02 +0000

    Convert ScreenPlay into installed editable package

---
+++ screenplay/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='screenplay',
+    version='1.0.0',
+    description='Screen play pattern in Python',
+    url='https://github.com/byran/Python-ScreenPlay',
+    author='Byran Wills-Heath',
+    author_email='byran@adgico.co.uk',
+    license='MIT',
+    packages=['screenplay'],
+    zip_safe=False)
