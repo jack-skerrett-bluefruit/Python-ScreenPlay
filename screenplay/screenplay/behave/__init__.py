2c88a73c75d4feeaaa92b5686fc57d314fd3d876
Byran Wills-Heath <byran@adgico.co.uk>
2020-02-29 16:50:26 +0000

    Add initial XML formatter for Screen play

--- screenplay/screenplay/behave/__init__.py
+++ screenplay/screenplay/behave/__init__.py
@@ -1,4 +1,5 @@
 from .actors import Actors
 from .context import add_screenplay_objects_to
+from .formatter import ScreenplayFormatter
 
-__all__ = ['Actors', 'add_screenplay_objects_to']
+__all__ = ['Actors', 'add_screenplay_objects_to', 'ScreenplayFormatter']
---
+++ screenplay/screenplay/behave/formatter.py
@@ -0,0 +1,47 @@
+from behave.formatter.base import Formatter
+from behave.model_core import Status
+from typing import IO, Any
+from xml.etree import ElementTree as ET
+import time
+
+
+class ScreenplayFormatter(Formatter):
+    def __init__(self, stream_opener, config):
+        super().__init__(stream_opener, config)
+        self.stream: IO[Any] = self.open()
+        self.steps = []
+        self.xml = ET.Element('results')
+
+    def feature(self, feature):
+        self.feature_xml = ET.SubElement(self.xml, 'feature')
+        self.feature_xml.attrib['name'] = feature.name
+        self.scenario_xml = None
+        self.step_xml = None
+        self.feature_start_time = time.time()
+
+    def scenario(self, scenario):
+        self.scenario_xml = ET.SubElement(self.feature_xml, 'scenario')
+        self.scenario_xml.attrib['name'] = scenario.name
+        self.scenario_xml.attrib['status'] = 'passed'
+        self.step_xml = None
+
+    def step(self, step):
+        self.steps.append(step)
+
+    def match(self, match):
+        step = self.steps.pop(0)
+
+        self.step_xml = ET.SubElement(self.scenario_xml, 'step')
+        self.step_xml.attrib['name'] = step.name
+
+    def result(self, step_result):
+        self.step_xml.attrib['status'] = step_result.status.name
+
+        if step_result.status.value != Status.passed:
+            self.scenario_xml.attrib['status'] = step_result.status.name
+
+    def eof(self):
+        self.feature_xml.attrib['time'] = str(time.time() - self.feature_start_time)
+
+    def close(self):
+        self.stream.write(ET.tostring(self.xml, encoding='UTF-8').decode('UTF-8'))
